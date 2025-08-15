9c652e7261338089afb8614a454cce3f6467b1e1
CodeTCreator <TTT>
2025-08-15 13:03:49 +0300

    Error models for requests and responses

---
+++ ServerAPI/DB/Request/ErrorGetRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerAPI.DB.Request
+{
+    public class ErrorGetRequest
+    {
+        [Key]
+        public string ErrorGUID { get; set; } = String.Empty;
+    }
+}
